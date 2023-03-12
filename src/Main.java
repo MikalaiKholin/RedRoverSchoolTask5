608621564347e7aa5df364904cabe79e9a07743a
MikalaiKholin <kholin.mikalai@gmail.com>
2023-03-12 23:44:47 +0300

    feat : all tasks complite

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 9 - i; j >= 0; j--){
+                System.out.printf("%d  ", 9 - j - i);
+            }
+            System.out.println();
+        }
+
+        System.out.println("======================================");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = i; j > 0; j--){
+                System.out.print("   ");
+            }
+
+            for (int j = 9 - i; j >= 0; j--){
+                System.out.printf("%d  ", 9 - j - i);
+            }
+            System.out.println();
+        }
+
+        System.out.println("======================================");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = i; j > 0; j--){
+                System.out.print("   ");
+            }
+
+            for (int j = 9 - i; j >= 0; j--){
+                System.out.printf("%d  ", j);
+            }
+
+            for (int j = 9 - i - 1; j >= 0; j--){
+                System.out.printf("%d  ", 9 - j - i);
+            }
+            System.out.println();
+        }
+
     }
 }
